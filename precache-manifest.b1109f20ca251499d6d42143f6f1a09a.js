ea6e0764aa568f7bfc0be3173b0d9f88070058a0
JSW <allmie@naver.com>
2019-11-16 01:04:08 +0900

    Updates

---
+++ precache-manifest.b1109f20ca251499d6d42143f6f1a09a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f392f4e1f93d2d0744b403e66f616d98",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "405656224ee5a0b0b091",
+    "url": "/movie_app/static/css/main.5a12e74e.chunk.css"
+  },
+  {
+    "revision": "fdebc56fd9c3ea6c5111",
+    "url": "/movie_app/static/js/2.1e7e776a.chunk.js"
+  },
+  {
+    "revision": "405656224ee5a0b0b091",
+    "url": "/movie_app/static/js/main.a13a984a.chunk.js"
+  },
+  {
+    "revision": "8971f8699b511f3e262b",
+    "url": "/movie_app/static/js/runtime-main.2890315e.js"
+  }
+]);
